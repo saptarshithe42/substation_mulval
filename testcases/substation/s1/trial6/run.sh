aaa4e6ac63744d7e05c8c475936b6e946706793c
saptarshithe42 <saptarshi.the.42@gmail.com>
2024-10-15 18:43:35 +0530

    ..

--- testcases/substation/s1/trial6/run.sh
+++ testcases/substation/s1/trial6/run.sh
@@ -5,20 +5,25 @@ programUsesLibrary="programUsesLibrary.txt"
 goal="goal.txt"
 haclRules="hacl_rules.txt"
 hostsInfo="hosts_info.txt"
-customRules="custom_rules.txt"
-
+customRules="custom_rules2.P"
 inputFile="input_file.P"
+runFiles="run_files"
 
-rm $inputFile
-touch $inputFile
+cd $runFiles
+
+>"$inputFile"
 
 cat $goal >>$inputFile
 cat $haclRules >>$inputFile
-cat $hostsInfo >>$inputFile
 cat $vulPropertyListFile >>$inputFile
 cat $vulExistsLibraryFile >>$inputFile
 cat $vulExistsProgramFile >>$inputFile
 cat $programUsesLibrary >>$inputFile
+cat $hostsInfo >>$inputFile
+
+cd ..
+
+mv ./$runFiles/$inputFile ./$inputFile
 
 source init_mulval_vars.sh
 
